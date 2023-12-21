d41f37320a1062dbd75f715aae121eb76c054276
Michael Ritter <info@michi-ritter.de>
2023-12-21 18:39:49 +0100

    Update directory-listing-tree.py

--- directory-listing-tree.py
+++ directory-listing-tree.py
@@ -1,40 +1,47 @@
 import argparse
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-import sys
+from urllib.parse import urljoin, urlparse
 from datetime import datetime
-import time
+import warnings
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# Suppress only the single InsecureRequestWarning from urllib3 needed
+warnings.simplefilter('ignore', InsecureRequestWarning)
 
 # ANSI escape codes for text color
 BLUE = '\033[94m'
 ORANGE = '\033[93m'
 RESET = '\033[0m'
 
-def is_directory_listing_link(link):
-    # Check if the link ends with '/' indicating it's a directory
-    return link.endswith('/')
+def is_directory_listing_page(soup):
+    if soup.find('title') and 'Index of' in soup.find('title').get_text():
+        return True
+    return False
 
-def extract_directory_name(link_text):
-    # Extract the directory name from the link text
-    return link_text.strip('/')
+def is_file_based_on_extension(url):
+    parsed_url = urlparse(url)
+    if '.' in parsed_url.path.split('/')[-1]:
+        return True
+    return False
 
 def get_file_info(file_url):
     try:
-        response = requests.head(file_url)
+        response = requests.get(file_url, verify=False, stream=True)
         response.raise_for_status()
-        
-        # Extract file size and convert it to MB
-        file_size_bytes = int(response.headers.get('Content-Length', '0'))
+
+        file_size_bytes = int(response.headers.get('Content-Length', 0))
         file_size_mb = file_size_bytes / (1024 * 1024)  # Convert to MB
-        
-        # Extract last modified date, or use "N/A" if not available
-        last_modified = response.headers.get('Last-Modified', 'N/A')
-        if last_modified != 'N/A':
-            last_modified_date = datetime.strptime(last_modified, '%a, %d %b %Y %H:%M:%S %Z')
-            formatted_last_modified = last_modified_date.strftime('%Y.%m.%d')
+
+        last_modified = response.headers.get('Last-Modified', None)
+        if last_modified:
+            try:
+                last_modified_date = datetime.strptime(last_modified, '%a, %d %b %Y %H:%M:%S %Z')
+                formatted_last_modified = last_modified_date.strftime('%Y.%m.%d')
+            except ValueError:
+                formatted_last_modified = "Unknown Date"
         else:
-            formatted_last_modified = 'N/A'
+            formatted_last_modified = "Not Available"
 
         return file_size_mb, formatted_last_modified
 
@@ -42,45 +49,68 @@ def get_file_info(file_url):
         print(f"Error fetching file info for {file_url}: {e}")
         return 'N/A', 'N/A'
 
-def fetch_directory_listing(url, indent=""):
+def fetch_directory_listing(url, soup):
+    items = []
+
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'html.parser')
+        rows = soup.find_all('tr')
+        for row in rows[2:]:
+            cols = row.find_all('td')
+            if len(cols) > 3:
+                name = cols[1].get_text(strip=True)
+                if name == 'Parent Directory':
+                    continue
 
-        parent_directory_found = False
+                href = cols[1].find('a')['href']
+                full_url = urljoin(url, href)
+                last_modified = cols[2].get_text(strip=True)
 
-        for link in soup.find_all('a', href=True):
-            href = link['href']
-            directory_name = extract_directory_name(link.get_text())
-
-            if parent_directory_found:
-                if is_directory_listing_link(href):
-                    # It's a directory (blue text)
-                    print(BLUE + indent + directory_name + RESET)
-                    fetch_directory_listing(urljoin(url, href), indent + '  ')
+                if is_file_based_on_extension(full_url):
+                    size_mb, last_modified = get_file_info(full_url)
+                    if isinstance(size_mb, float):
+                        details = f"Size: {size_mb:.3f} MB, Last Modified: {last_modified}"
+                    else:
+                        details = f"Size: {size_mb}, Last Modified: {last_modified}"
+                    items.append((False, name, details))
                 else:
-                    # It's a file (orange text)
-                    file_url = urljoin(url, href)
-                    file_size_mb, last_modified = get_file_info(file_url)
-                    file_size_text = "%.2f MB" % file_size_mb if file_size_mb != 'N/A' else 'N/A'
-                    print(ORANGE + indent + "%s  Size: %s, Last Modified: %s" % (directory_name, file_size_text, last_modified) + RESET)
-
-            elif directory_name == 'Parent Directory':
-                # Found the "Parent directory" link
-                parent_directory_found = True
+                    details = f"Last Modified: {last_modified}"
+                    items.append((True, name, details))
 
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
 
+    return items
+
+def print_directory_listing(url, indent=""):
+    try:
+        response = requests.get(url, verify=False)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        if is_directory_listing_page(soup):
+            items = fetch_directory_listing(url, soup)
+            items.sort(key=lambda x: (not x[0], x[1]))
+
+            for is_dir, name, details in items:
+                if is_dir:
+                    print(BLUE + indent + name + RESET)
+                    next_url = urljoin(url, name + '/')
+                    print_directory_listing(next_url, indent + '    ')
+                else:
+                    print(ORANGE + indent + f"{name}  {details}" + RESET)
+        else:
+            print(f"{indent}The page at {url} is not a directory listing. Skipping.")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error accessing {url}: {e}")
+
 def main():
     parser = argparse.ArgumentParser(description="Fetch and display files in a directory listing URL")
-    parser.add_argument("--url", required=True, help="The URL with directory listing")
+    parser.add_argument("--url", required=True, help="A URL with directory listing")
 
     args = parser.parse_args()
 
-    url = args.url
-    fetch_directory_listing(url)
+    print_directory_listing(args.url)
 
 if __name__ == "__main__":
     main()
